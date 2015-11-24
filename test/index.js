401b57f232cdf3dc5548bc1a8b59a91cd62cad39
Ricky Reusser <rsreusser@gmail.com>
2015-11-24 08:28:12 -0500

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,12 @@
+/* global describe, it */
+
+'use strict'
+
+// import locateEquationsInString from '../src'
+import { assert } from 'chai'
+
+describe('locateEquationsInString', () => {
+  it('works', () => {
+    assert(true)
+  })
+})
